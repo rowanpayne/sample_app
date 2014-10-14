3e6cee7f3bd026e9f20e74d9f4ac52d8328863a6
Rowan Payne <rowan.payne@gmail.com>
2014-10-14 22:06:43 +1300

    setting up testing with guard and spork

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,7 +1,11 @@
 require 'spec_helper'
 
 
+
 describe "Static pages" do 
+  
+  let(:title_stub) {title_stub = "Ruby on Rails Tutorial Sample App"}
+
   describe "Home page" do
     it "Should have the content 'Sample App'" do 
       visit '/static_pages/home'
@@ -9,7 +13,7 @@ describe "Static pages" do
     end
     it "Should have the title 'Home'" do
       visit '/static_pages/home'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+      expect(page).to have_title("#{title_stub} | Home")
     end
   end
 
@@ -20,7 +24,7 @@ describe "Static pages" do
   	end
     it "Should have the title 'Help'" do
       visit '/static_pages/help'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+      expect(page).to have_title("#{title_stub} | Help")
     end
   end
 
@@ -31,7 +35,7 @@ describe "Static pages" do
     end
     it "Should have the title 'About Us'" do
       visit '/static_pages/about'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+      expect(page).to have_title("#{title_stub} | About Us")
     end
   end
 
@@ -42,7 +46,7 @@ describe "Static pages" do
     end
     it "Should have the title 'Contact'" do
       visit '/static_pages/contact'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact")
+      expect(page).to have_title("#{title_stub} | Contact")
     end
   end
 
